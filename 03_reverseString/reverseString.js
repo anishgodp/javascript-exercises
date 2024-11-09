b46e532c6345085893160b3775bd337bb0a06c97
anishgodp <157295383+anishgodp@users.noreply.github.com>
2024-11-09 11:19:54 +0000

    added input variable and returned input which has been split into an array reveresed and joined

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function () {
+	// create a variable for the input
+	let input = arguments[0];
 
+	// return input which has been split into an array, reversed and joined
+	return input.split("").reverse().join("");
 };
 
 // Do not edit below this line

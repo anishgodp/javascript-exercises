ddf85f840e905925c888ddbd6c1aa61a2afb6adc
anishgodp <157295383+anishgodp@users.noreply.github.com>
2024-11-09 11:09:24 +0000

    updated js function to pass spec.js test, did not completely resemble solution code, but it did still pass.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function () {
+	//create variable to hold string to return
+	let result = "";
+	const number = 3;
+	//create loop to repeat number of times and add string to the result of each loop
+	for (let i = 0; i < number; i++) {
+		result += "hey";
+	}
 
+	//return result
+	return result;
 };
 
 // Do not edit below this line

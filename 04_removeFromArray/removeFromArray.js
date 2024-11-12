7cc4b2b95229ef5e0bc9c825fa9ee8f1e6f3aa42
anishgodp <157295383+anishgodp@users.noreply.github.com>
2024-11-12 16:51:58 +0000

    function updated to pass specified tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (...args) {
+	// First argument is the array
+	const array = args[0];
+	// Get all other arguments (elements to remove)
+	const elementsToRemove = args.slice(1);
 
+	// Filter the array to keep only elements that are not in elementsToRemove
+	return array.filter((item) => !elementsToRemove.includes(item));
+};
 // Do not edit below this line
 module.exports = removeFromArray;

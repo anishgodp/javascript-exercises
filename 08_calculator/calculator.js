d3696bd9dd4b7378473f97fbae10245bc7893eeb
anishgodp <157295383+anishgodp@users.noreply.github.com>
2024-11-13 15:59:44 +0000

    functions updated to complete function requirements

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,47 @@
-const add = function() {
-	
+const add = function (a, b) {
+	// function to add two numbers
+	const result = a + b;
+	return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	// function to subtract two numbers
+	const result = a - b;
+	return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	// function to sum an array
+	const result = array.reduce((total, current) => total + current, 0);
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	// function to multiply two numbers
+	return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	// function to raise one number to the power of another
+	const result = Math.pow(a, b);
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	// function to compute the factorial of a number
+	let product = 1;
+	for (let i = n; i > 0; i--) {
+		product *= i;
+	}
+	return product;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };

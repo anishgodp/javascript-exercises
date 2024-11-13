c3a5dbabafe97c2b3f2afa431880a32143f34f0b
anishgodp <157295383+anishgodp@users.noreply.github.com>
2024-11-13 15:39:03 +0000

    functions updated to convert celcius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+// write two functions that convert between celsius and fahrenheit and vice versa
+// round result to 1 decimal
+
+const convertToCelsius = function (number) {
+	// convert a given number from fahrenheit to celsius
+	let fahrenheit = number;
+	let celsius = (fahrenheit - 32) * (5 / 9);
+	return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (number) {
+	// convert a given number from celsius to fahrenheit
+	let celsius = number;
+	let fahrenheit = (celsius * 9) / 5 + 32;
+	return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
